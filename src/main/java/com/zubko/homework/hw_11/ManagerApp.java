fbdf97bbe1eff6f77075edb30fb7a4523bc33e93
Anna Zubko <annazubko1986@gmail.com>
2020-04-27 00:18:51 +0300

    homework 11

---
+++ src/main/java/com/zubko/homework/hw_11/ManagerApp.java
@@ -0,0 +1,17 @@
+package com.zubko.homework.hw_11;
+
+public class ManagerApp {
+    public static void main(String[] args) {
+        Url url = new Url.UrlBuilder()
+                .setProtocol("https")
+                .setDomain("google.com")
+                .setPort("8800")
+                .setPath("getSomeInfo")
+                .setParam("name", "Pete")
+                .setParam("surname", "Some")
+                .build();
+        System.out.println(url.getUrl());
+
+
+    }
+}
